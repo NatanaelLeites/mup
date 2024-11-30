a4240f7fbd279b2d375bd7578f2250e94c120aa9
natanael <nathaxx09@hotmail.com>
2024-11-30 00:37:14 -0300

    correccion de registro.js

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,26 +1,4 @@
-/* Firebase */
-/* import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
 
-import {
-  sendEmailVerification, getAuth, signInWithPopup,
-  createUserWithEmailAndPassword, signInWithEmailAndPassword,
-  onAuthStateChanged
-} from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBdxFOSuXhzpaRXYLDC5Zi6yR_wwS42tSU",
-  authDomain: "mupoficial-19599.firebaseapp.com",
-  projectId: "mupoficial-19599",
-  storageBucket: "mupoficial-19599.firebasestorage.app",
-  messagingSenderId: "219106381952",
-  appId: "1:219106381952:web:dab8f20f21bd2aba5e9665",
-  measurementId: "G-1QMM0TTZGF"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app); */
-
-/* Fin codigo Firebase */
 
 /* Logica para formularios */
 const registroForm = document.getElementById('registroForm')
